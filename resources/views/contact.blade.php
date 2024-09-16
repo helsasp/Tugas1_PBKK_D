a27a0f83798968c6333ec97a9fcc540b68b5f26e
Helsa Sriprameswari Putri <144691463+helsasp@users.noreply.github.com>
2024-09-16 17:37:17 +0700

    Add files via upload

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <x-layout>
 <x-slot:title>{{ $title }}</x-slot:title>
   <h3 class="text-xl">Welcome to Contact Page! <3</h3>
@@ -20,4 +21,28 @@
     </li>
 </ul>
 
+=======
+<x-layout>
+<x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">Welcome to Contact Page! <3</h3>
+  <p>Please reach out on one of my social media :</p>
+  <ul>
+    <li>
+        <a href="https://www.instagram.com/helsa__putri/" target="_blank" style="text-decoration: underline;">
+            <i class="fab fa-instagram"></i> Instagram
+        </a>
+    </li>
+    <li>
+        <a href="https://www.linkedin.com/in/helsa-putri-21b38424a/" target="_blank" style="text-decoration: underline;">
+            <i class="fab fa-linkedin"></i> LinkedIn
+        </a>
+    </li>
+    <li>
+        <a href="https://github.com/helsasp" target="_blank" style="text-decoration: underline;">
+            <i class="fab fa-github"></i> GitHub
+        </a>
+    </li>
+</ul>
+
+>>>>>>> 1ae10c5 (Initial commit)
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
   <h3 class="text-xl">Welcome to Helsa's Home Page! <3</h3>
@@ -7,4 +8,15 @@
     <li>2. About</li>
     <li>3. Contact</li>
   </ul>
+=======
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">Welcome to Helsa's Home Page! <3</h3>
+  <p>In this home page, you can find some of these menu : </p>
+  <ul>
+    <li>1. Blog</li>
+    <li>2. About</li>
+    <li>3. Contact</li>
+  </ul>
+>>>>>>> 1ae10c5 (Initial commit)
 </x-layout>
