8173c352d60d37be2d9a97968a4a03c5ecef7376
helsasp <helsasp@gmail.com>
2024-09-20 12:41:30 +0700

    fixing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
   <h3 class="text-xl">Welcome to Helsa's Home Page! <3</h3>
@@ -8,15 +7,4 @@
     <li>2. About</li>
     <li>3. Contact</li>
   </ul>
-=======
-<x-layout>
-  <x-slot:title>{{ $title }}</x-slot:title>
-  <h3 class="text-xl">Welcome to Helsa's Home Page! <3</h3>
-  <p>In this home page, you can find some of these menu : </p>
-  <ul>
-    <li>1. Blog</li>
-    <li>2. About</li>
-    <li>3. Contact</li>
-  </ul>
->>>>>>> 0abf12214546c23f8da091a725a676463d1660e9
 </x-layout>
